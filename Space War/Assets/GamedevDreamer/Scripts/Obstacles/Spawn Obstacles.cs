a13df22c6a8d2e4be8abe16590f92179b700cfec
Henry Erondu <119769018+henchmotion@users.noreply.github.com>
2024-08-06 08:50:13 +0100

    Spawn Obstacles and Game over
    I worked on the spawning Obstacles and also updated the game over panel which houses the game over and text and restart button

--- Space War/Assets/GamedevDreamer/Scripts/Obstacles/Spawn Obstacles.cs
+++ Space War/Assets/GamedevDreamer/Scripts/Obstacles/Spawn Obstacles.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class SpawnObstacles : MonoBehaviour
 {
-    public GameObject Obstacle;
-    public  float maxX;
-    public  float minX;
-    public  float maxY;
-    public float minY;
-    public  float timeBetweenSpawn;
+    public GameObject obstacle;
+    public float maxX;
+    public float minX;
+    public float maxY;
+    public float  minY;
+    public float timeBetweenSpawn;
     private float spawnTime;
-   
+
+    // Update is called once per frame
     void Update()
     {
         if(Time.time > spawnTime)
@@ -21,12 +22,11 @@ public class SpawnObstacles : MonoBehaviour
         }
     }
 
-    private void Spawn()
+    void Spawn()
     {
         float randomX = Random.Range(minX, maxX);
         float randomY = Random.Range(minY, maxY);
 
-        Instantiate(Obstacle, transform.position + new Vector3(randomX, randomY, 0), transform.rotation);
-
+        Instantiate(obstacle, transform.position + new Vector3(randomX, randomY, 0), transform.rotation);
     }
 }
